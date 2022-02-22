10650d67df1151fa8a22d7672ddce3dbaf84c35c
ryoneda <ryo.roma.16@gmail.com>
2022-02-22 22:43:32 +0900

    基準を4.5σに変更

--- environment.js
+++ environment.js
@@ -392,7 +392,7 @@ io.on('connection', function(socket){
           //test用
           //status_count += 1;
           //sdnnが大きいとマインドワンダリング
-          if(baseline_ave+baseline_sd*3 < sdnn){
+          if(baseline_ave+baseline_sd*4.5 < sdnn){
             status_count += 1;
             console.log('MW');
           }else{
